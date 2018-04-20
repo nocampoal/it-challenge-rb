74e98bffb500b4ef3de4636c9f859e41ca254353
nocampo-technisys <nocampo@technisys.com>
2018-04-20 11:00:37 -0300

    add components angular

--- src/main/resources/webapp/src/app/app.module.ts
+++ src/main/resources/webapp/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LocationsComponent } from './components/locations/locations.component';
+import { AddLocationComponent } from './components/add-location/add-location.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocationsComponent,
+    AddLocationComponent
   ],
   imports: [
     BrowserModule
